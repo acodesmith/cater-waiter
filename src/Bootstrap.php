5f8c3fc590a57aa05a87440bbd5ab8061eb8ef65
acodesmith <adam@acodesmith.com>
2017-10-19 13:17:05 -0400

    styling the product panel option

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -8,6 +8,10 @@ use CaterWaiter\Admin\WC_ProductDataTabs;
 
 class Bootstrap
 {
+    const DEP_ADMIN_SCRIPT  = 'cater_waiter_admin_script';
+
+    const DEP_ADMIN_STYLE   = 'cater_waiter_admin_style';
+
     public function __construct()
     {
         add_action( 'plugins_loaded', [ $this, 'run' ] );
@@ -26,6 +30,7 @@ class Bootstrap
 
     public function admin_scripts()
     {
-
+        wp_enqueue_script( self::DEP_ADMIN_SCRIPT, CATER_WAITER_PLUGIN_URL . '/dist/admin.js', [ 'jquery' ], '0.0.1', true );
+        wp_enqueue_style( self::DEP_ADMIN_STYLE, CATER_WAITER_PLUGIN_URL . '/dist/admin.css', [], '0.0.1' );
     }
 }
