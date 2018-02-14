c6ec50ddf570c5a40b119fe9f044548e5cd44ad8
acodesmith <adam@acodesmith.com>
2018-02-14 10:35:49 -0500

    Delivery Location Logic #8. Adding svg icon.

--- src/Module/WooCommerce/Order.php
+++ src/Module/WooCommerce/Order.php
@@ -35,18 +35,37 @@ class Order {
 
 				add_post_meta( $order_id, 'order_pickup_time', $order_date );
 			}
+
+			// Set Order Deliver Address
+			if( ! empty( $order_info['order_delivery_address'] ) ) {
+
+				$order_info['order_delivery_address'] = (array) $order_info['order_delivery_address'];
+				$order_delivery_address_raw = $order_info['order_delivery_address'];
+
+				unset($order_info['order_delivery_address']['delivery_within_range']);
+
+				if( ! empty( $order_info['order_delivery_address']['delivery_address_line_two'] ) )
+					$order_info['order_delivery_address']['delivery_address_line_one'] .= ' ' . $order_info['order_delivery_address']['delivery_address_line_two'];
+
+
+				$order_info['order_delivery_address']['delivery_address_line_one'] .= ',';
+				$order_delivery_address = implode(' ', $order_info['order_delivery_address']);
+
+				add_post_meta( $order_id, 'order_delivery_address', $order_delivery_address );
+				add_post_meta( $order_id, 'order_delivery_address_json', json_encode( $order_delivery_address_raw ) );
+			}
 		}
 	}
 
 	/**
-	 * @todo Delivery Address
 	 * @param $order
 	 */
 	public function order_info_admin( $order ) {
 
-		$order_type         = get_post_meta( $order->get_id(), 'order_type', true );
-		$order_location     = get_post_meta( $order->get_id(), 'order_location_id', true );
-		$order_pickup_time  = get_post_meta( $order->get_id(), 'order_pickup_time', true );
+		$order_type             = get_post_meta( $order->get_id(), 'order_type', true );
+		$order_location         = get_post_meta( $order->get_id(), 'order_location_id', true );
+		$order_pickup_time      = get_post_meta( $order->get_id(), 'order_pickup_time', true );
+		$order_delivery_address = get_post_meta( $order->get_id(), 'order_delivery_address', true );
 
 		if ( ! empty( $order_type ) ) {
 			echo "<h3>Order Type</h3>" . ucwords( $order_type );
@@ -54,6 +73,10 @@ class Order {
 
 		if ( ! empty( $order_pickup_time ) ) {
 			echo "<h3>Order Time</h3>" . date( 'Y-m-d g:i A', strtotime( $order_pickup_time ) );
+		}
+
+		if( ! empty( $order_delivery_address ) ) {
+			echo "<h3>Order Time</h3>$order_delivery_address";
 		}
 
 		if ( ! empty( $order_location ) ) {
@@ -72,31 +95,39 @@ class Order {
 	}
 
 	/**
-	 * @todo Delivery Address
 	 * @param $order
 	 */
 	public function order_info_front_end($order) {
 
 		echo "<div class='cw__order_info'>";
 
-		$order_type         = get_post_meta( $order->get_id(), 'order_type', true );
-		$order_location     = get_post_meta( $order->get_id(), 'order_location_id', true );
-		$order_pickup_time  = get_post_meta( $order->get_id(), 'order_pickup_time', true );
+		$order_type             = get_post_meta( $order->get_id(), 'order_type', true );
+		$order_location         = get_post_meta( $order->get_id(), 'order_location_id', true );
+		$order_pickup_time      = get_post_meta( $order->get_id(), 'order_pickup_time', true );
+		$order_delivery_address = get_post_meta( $order->get_id(), 'order_delivery_address', true );
 
 		if ( ! empty( $order_type ) ) {
-			echo "<h3>Order Type</h3>" . ucwords( $order_type );
+			$label = __('Order Type', 'cater_waiter');
+			echo "<h3>$label</h3>" . ucwords( $order_type );
 		}
 
 		if ( ! empty( $order_pickup_time ) ) {
-			echo "<h3>Order Time</h3>" . date( 'Y-m-d g:i A', strtotime( $order_pickup_time ) );
+			$label = __('Order Time', 'cater_waiter');
+			echo "<h3>$label</h3>" . date( 'Y-m-d g:i A', strtotime( $order_pickup_time ) );
+		}
+
+		if( ! empty( $order_delivery_address ) ) {
+			$label = __('Order Delivery Address', 'cater_waiter');
+			echo "<h3>$label</h3>$order_delivery_address";
 		}
 
 		if ( ! empty( $order_location ) ) {
 			if ( $order_location = get_post( $order_location ) ) {
 
+				$label = __('Order Location', 'cater_waiter');
 				$order_location_meta = get_post_meta( $order_location->ID );
 
-				echo "<h3>Order Location</h3>";
+				echo "<h3>$label</h3>";
 				echo "$order_location->post_title<br />";
 				echo $order_location_meta['wf_address_one'][0] . "<br />";
 				echo $order_location_meta['wf_city'][0] . ", " . $order_location_meta['wf_state'][0] . " " . $order_location_meta['wf_zip'][0];
